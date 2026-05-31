b25b40c25c59b26ab442d2743e53bc92700747aa
Felipe César <www.felipecesargomes@gmail.com>
2026-05-31 11:12:16 -0300

    criação do review command controller

--- command/src/main/java/com/devdeolho/controller/ReviewCommandController.java
+++ command/src/main/java/com/devdeolho/controller/ReviewCommandController.java
@@ -1,4 +1,23 @@
 package com.devdeolho.controller;
 
+import com.devdeolho.bus.CommandBus;
+import com.devdeolho.domain.command.CreateReviewCommand;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/reviews")
 public class ReviewCommandController {
+
+    @Autowired
+    private CommandBus commandBus;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void createReview(@RequestBody @Valid CreateReviewCommand createReviewCommand) {
+        commandBus.execute(createReviewCommand);
+    }
+
 }
