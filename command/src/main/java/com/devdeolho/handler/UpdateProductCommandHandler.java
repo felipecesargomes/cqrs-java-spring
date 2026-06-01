94e69c57e869dd053612d387efb30baafb8356a2
Felipe César <www.felipecesargomes@gmail.com>
2026-06-01 12:16:18 -0300

    adicionado tobuilder para o handle

--- command/src/main/java/com/devdeolho/handler/UpdateProductCommandHandler.java
+++ command/src/main/java/com/devdeolho/handler/UpdateProductCommandHandler.java
@@ -1,4 +1,28 @@
 package com.devdeolho.handler;
 
-public class UpdateProductCommandHandler {
+import com.devdeolho.domain.Product;
+import com.devdeolho.domain.command.UpdateProductCommand;
+import com.devdeolho.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UpdateProductCommandHandler implements CommandHandler<UpdateProductCommand> {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Override
+    public void handle(UpdateProductCommand command) {
+        Product productToBeSaved = productRepository.findById(command.getId())
+                .orElseThrow(IllegalArgumentException::new)
+                .toBuilder()
+                .imageUrl(command.getImageUrl())
+                .name(command.getName())
+                .description(command.getDescription())
+                .value(command.getValue())
+                .build();
+
+        productRepository.save(productToBeSaved);
+    }
 }
